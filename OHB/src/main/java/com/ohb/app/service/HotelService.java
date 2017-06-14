f38848c058196df36fbbd6983ce514a826ea29a0
vishalvyas117 <vishalvyas117@gmail.com>
2017-06-14 22:10:43 +0530

    Update Hotel Reposiory

--- OHB/src/main/java/com/ohb/app/service/HotelService.java
+++ OHB/src/main/java/com/ohb/app/service/HotelService.java
@@ -28,30 +28,72 @@ public class HotelService {
 
 	@Autowired
 	RoomTypeRepository roomTypeRepository;
-	
-	
-	public Hotel createHotel(Hotel hotel){
-		Hotel dto=new Hotel();
-		if(hotel.getAddress()!=null){
+
+	public Hotel createHotel(Hotel hotel) {
+		Hotel dto = new Hotel();
+		if (hotel.getAddress() != null) {
 			dto.setAddress(hotel.getAddress());
-		}if(hotel.getName()!=null){
+		}
+		if (hotel.getName() != null) {
 			dto.setName(hotel.getName());
-		}if(hotel.getRating()>0){
+		}
+		if (hotel.getRating() > 0) {
 			dto.setRating(hotel.getRating());
-		}else{
+		} else {
 			dto.setRating(1);
 		}
 		dto.setCity(hotel.getCity());
 		dto.setCategory(hotel.getCategory());
-		Hotel outHotel=this.hotelRepository.save(dto);
-		if(outHotel==null){
+		Hotel outHotel = this.hotelRepository.save(dto);
+		if (outHotel == null) {
 			return null;
 		}
 		return outHotel;
 	}
-	
-	public List<Hotel>findAllforUser(){
+
+	public List<Hotel> findAllforUser() {
 		List<Hotel> page = this.hotelRepository.findtop3HotelsforeachCity();
+
+		List<Hotel> hotellist = new ArrayList<Hotel>();
+		for (Hotel hotel : page) {
+			Hotel dto = new Hotel();
+			dto.setHotelid(hotel.getHotelid());
+			dto.setName(hotel.getName());
+			dto.setRating(hotel.getRating());
+			dto.setCategory(hotel.getCategory());
+			dto.setAddress(hotel.getAddress());
+			dto.setImages(hotel.getImages());
+			dto.setCity(hotel.getCity());
+			dto.setRooms(hotel.getRooms());
+			hotellist.add(dto);
+		}
+		return hotellist;
+	}
+
+	public List<Hotel> findAll() {
+		List<Hotel> page = this.hotelRepository.findAll();
+
+		List<Hotel> hotellist = new ArrayList<Hotel>();
+		for (Hotel hotel : page) {
+			Hotel dto = new Hotel();
+			dto.setHotelid(hotel.getHotelid());
+			dto.setName(hotel.getName());
+			dto.setRating(hotel.getRating());
+			dto.setCategory(hotel.getCategory());
+			dto.setAddress(hotel.getAddress());
+			dto.setImages(hotel.getImages());
+			dto.setCity(hotel.getCity());
+			dto.setRooms(hotel.getRooms());
+			hotellist.add(dto);
+		}
+		return hotellist;
+	}
+
+	public List<Hotel> findHotelsByCity(Integer city_id) {
+		if(city_id==null){
+			city_id=0;
+		}
+		List<Hotel> page = this.hotelRepository.findHotelsByCity(city_id);
 
 		List<Hotel> hotellist = new ArrayList<Hotel>();
 		for (Hotel hotel : page) {
