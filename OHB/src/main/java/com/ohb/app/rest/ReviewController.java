c5de24769f7dca498d2d62ed2e124fbbc1d91035
vishalvyas117 <vishalvyas117@gmail.com>
2017-06-20 17:23:55 +0530

    addd Review Controller

---
+++ OHB/src/main/java/com/ohb/app/rest/ReviewController.java
@@ -0,0 +1,13 @@
+package com.ohb.app.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ohb.app.util.api.APIName;
+import com.ohb.app.util.api.APIUtil;
+@RestController
+@RequestMapping(APIName.REVIEWS)
+public class ReviewController extends APIUtil{
+
+	
+}
